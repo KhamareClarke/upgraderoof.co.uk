0ca228762ad66f030133b3623d050eace1995b9a
Khamare Clarke <khamareclarke@gmail.com>
2026-03-17 20:21:22 +0000

    Remove experimental optimizeCss flag to fix Vercel build

--- next.config.js
+++ next.config.js
@@ -9,9 +9,6 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
     minimumCacheTTL: 31536000,
   },
-  experimental: {
-    optimizeCss: true,
-  },
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
